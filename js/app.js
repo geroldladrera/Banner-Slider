c178f75762ad7024550b47a9bfd63591c87830c3
Gerold Ladrera <gerold.ladrera@realpage.com>
2022-05-27 00:58:30 +0800

    Frontend - Banner Slider or Carousel

---
+++ js/app.js
@@ -0,0 +1,42 @@
+let slideIndex = 0;
+
+setTimeout(showSlides, 3000);
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";   
+    }
+    
+    slideIndex++;
+
+    if (slideIndex > slides.length) {
+        slideIndex = 1
+        
+    }
+
+    slides[slideIndex-1].style.display = "block";  
+
+    setTimeout(showSlides, 1000); // Change image every 1 second
+}
+
+fetch("https://fakestoreapi.com/products").then((data)=>{
+    return data.json();
+}).then((completedata)=>{
+    console.log(completedata);
+    let data = "";
+    completedata.map((values)=>{
+        data += `
+            <div class="mySlides fade">
+                <div class="numbertext">${values.id} / ${Object.keys(completedata).length}</div>
+                <img src="${values.image}" alt="img-slider" style="width:100%">
+                <div class="text">${values.title}</div>
+            </div>
+        `
+    });
+    document.getElementById("mySlider").innerHTML = data;
+}).catch((err)=>{
+    console.log(err);    
+})
